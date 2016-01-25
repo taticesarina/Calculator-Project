856af8fe8d4887e726c2c1cf4f4fc738e3aaf22c
taticesarina <tatiana.infante@mihsnyc.org>
2016-01-25 14:32:02 +0000

    added functions to my calculator

--- calculator.js
+++ calculator.js
@@ -133,4 +133,39 @@ function divide(x,y)
 {
   return x/y;
 }
+
+function multiply(x,y)
+{
+	return x*y;
+}
+
+function subtract(x,y)
+{
+	return x-y;
+}
+
+function add(x,y)
+{
+	return x+y;
+}
+
+function square_root(x)
+{
+	return Math.sqrt(x);
+}
+
+function power(x,y)
+{
+	return Math.pow(4, 3);
+}
+
+function logBase10(x)
+{
+	return  Math.log10() 
+}
+
+function generateRandomNumber()
+{
+	return Math.random();
+}
 // You must implement the rest of the functions here.
